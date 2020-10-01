63763eb5a5c0e691d1bdfe7ccf01259e0d047104
Diego <diegof.df58@gmail.com>
2020-10-01 16:53:51 -0300

    01102020

---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0px;
+  font-size:30px;
+  img {
+    margin-right: 10px;
+    height: 80px;
+    width: 80px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 100%;
+    margin-bottom: -8px;
+  }
+`
+
+const UserReviewCount = styled.div`
+  font-size: 18px;
+  padding:10px 0;
+`
+
+const ScoreOutOf = styled.div`
+  padding: 10px 0;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const Header = (props) => {
+    const {name, image_url, avg_score} = props.attributes
+    const total = props.reviews.length
+
+    return (
+        <Wrapper>
+            <h1><img src={image_url} alt={name}></img>{name}</h1>
+            <div>
+               <UserReviewCount>{total} User Reviews</UserReviewCount>
+               <div className="starRating"></div>
+               <ScoreOutOf>{avg_score} out of 5</ScoreOutOf>
+            </div>
+        </Wrapper>
+    )
+}
+
+export default Header
